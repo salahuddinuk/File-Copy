e47b839c4976056a8ceff7e31b17dc9baef8e52f
salahuddinuk <salahuddin.uk@gmail.com>
2018-10-11 07:52:29 +0400

    plain input path file

---
+++ AutoFileCopyApplication/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace AutoFileCopyApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_FileName = new System.Windows.Forms.TextBox();
+            this.txt_InputPath = new System.Windows.Forms.TextBox();
+            this.txt_OutputPath = new System.Windows.Forms.TextBox();
+            this.lbl_FileName = new System.Windows.Forms.Label();
+            this.lbl_InputPath = new System.Windows.Forms.Label();
+            this.lbl_OutputPath = new System.Windows.Forms.Label();
+            this.lb_Status = new System.Windows.Forms.ListBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_DialogInput = new System.Windows.Forms.Button();
+            this.btn_DialogOutput = new System.Windows.Forms.Button();
+            this.folderBrowserDialog_Input = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog_Output = new System.Windows.Forms.FolderBrowserDialog();
+            this.lb_Error = new System.Windows.Forms.ListBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_FileName
+            // 
+            this.txt_FileName.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_FileName.Enabled = false;
+            this.txt_FileName.Location = new System.Drawing.Point(109, 18);
+            this.txt_FileName.Name = "txt_FileName";
+            this.txt_FileName.Size = new System.Drawing.Size(260, 20);
+            this.txt_FileName.TabIndex = 0;
+            this.txt_FileName.Text = "*.bak";
+            // 
+            // txt_InputPath
+            // 
+            this.txt_InputPath.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_InputPath.Enabled = false;
+            this.txt_InputPath.Location = new System.Drawing.Point(109, 44);
+            this.txt_InputPath.Name = "txt_InputPath";
+            this.txt_InputPath.ReadOnly = true;
+            this.txt_InputPath.Size = new System.Drawing.Size(260, 20);
+            this.txt_InputPath.TabIndex = 1;
+            // 
+            // txt_OutputPath
+            // 
+            this.txt_OutputPath.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_OutputPath.Enabled = false;
+            this.txt_OutputPath.Location = new System.Drawing.Point(109, 71);
+            this.txt_OutputPath.Name = "txt_OutputPath";
+            this.txt_OutputPath.ReadOnly = true;
+            this.txt_OutputPath.Size = new System.Drawing.Size(260, 20);
+            this.txt_OutputPath.TabIndex = 2;
+            // 
+            // lbl_FileName
+            // 
+            this.lbl_FileName.AutoSize = true;
+            this.lbl_FileName.Location = new System.Drawing.Point(19, 21);
+            this.lbl_FileName.Name = "lbl_FileName";
+            this.lbl_FileName.Size = new System.Drawing.Size(54, 13);
+            this.lbl_FileName.TabIndex = 3;
+            this.lbl_FileName.Text = "File Name";
+            // 
+            // lbl_InputPath
+            // 
+            this.lbl_InputPath.AutoSize = true;
+            this.lbl_InputPath.Location = new System.Drawing.Point(19, 47);
+            this.lbl_InputPath.Name = "lbl_InputPath";
+            this.lbl_InputPath.Size = new System.Drawing.Size(56, 13);
+            this.lbl_InputPath.TabIndex = 4;
+            this.lbl_InputPath.Text = "Input Path";
+            // 
+            // lbl_OutputPath
+            // 
+            this.lbl_OutputPath.AutoSize = true;
+            this.lbl_OutputPath.Location = new System.Drawing.Point(19, 78);
+            this.lbl_OutputPath.Name = "lbl_OutputPath";
+            this.lbl_OutputPath.Size = new System.Drawing.Size(64, 13);
+            this.lbl_OutputPath.TabIndex = 5;
+            this.lbl_OutputPath.Text = "Output Path";
+            // 
+            // lb_Status
+            // 
+            this.lb_Status.FormattingEnabled = true;
+            this.lb_Status.Location = new System.Drawing.Point(22, 105);
+            this.lb_Status.Name = "lb_Status";
+            this.lb_Status.Size = new System.Drawing.Size(476, 290);
+            this.lb_Status.TabIndex = 6;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_Start.Location = new System.Drawing.Point(414, 18);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(84, 74);
+            this.btn_Start.TabIndex = 7;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Visible = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_DialogInput
+            // 
+            this.btn_DialogInput.Enabled = false;
+            this.btn_DialogInput.Location = new System.Drawing.Point(370, 43);
+            this.btn_DialogInput.Name = "btn_DialogInput";
+            this.btn_DialogInput.Size = new System.Drawing.Size(26, 22);
+            this.btn_DialogInput.TabIndex = 9;
+            this.btn_DialogInput.Text = "...";
+            this.btn_DialogInput.UseVisualStyleBackColor = true;
+            this.btn_DialogInput.Click += new System.EventHandler(this.btn_DialogInput_Click);
+            // 
+            // btn_DialogOutput
+            // 
+            this.btn_DialogOutput.Enabled = false;
+            this.btn_DialogOutput.Location = new System.Drawing.Point(370, 70);
+            this.btn_DialogOutput.Name = "btn_DialogOutput";
+            this.btn_DialogOutput.Size = new System.Drawing.Size(26, 22);
+            this.btn_DialogOutput.TabIndex = 10;
+            this.btn_DialogOutput.Text = "...";
+            this.btn_DialogOutput.UseVisualStyleBackColor = true;
+            this.btn_DialogOutput.Click += new System.EventHandler(this.btn_DialogOutput_Click);
+            // 
+            // lb_Error
+            // 
+            this.lb_Error.BackColor = System.Drawing.Color.NavajoWhite;
+            this.lb_Error.FormattingEnabled = true;
+            this.lb_Error.Location = new System.Drawing.Point(22, 399);
+            this.lb_Error.Name = "lb_Error";
+            this.lb_Error.Size = new System.Drawing.Size(476, 43);
+            this.lb_Error.TabIndex = 11;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Minimize to tray";
+            this.notifyIcon1.BalloonTipTitle = "SQL Backup File Copier";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SQL Backup File Copier";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 464);
+            this.Controls.Add(this.lb_Error);
+            this.Controls.Add(this.btn_DialogOutput);
+            this.Controls.Add(this.btn_DialogInput);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lb_Status);
+            this.Controls.Add(this.lbl_OutputPath);
+            this.Controls.Add(this.lbl_InputPath);
+            this.Controls.Add(this.lbl_FileName);
+            this.Controls.Add(this.txt_OutputPath);
+            this.Controls.Add(this.txt_InputPath);
+            this.Controls.Add(this.txt_FileName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(538, 503);
+            this.Name = "Form1";
+            this.Text = "SQL Backup File Copier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_FileName;
+        private System.Windows.Forms.TextBox txt_InputPath;
+        private System.Windows.Forms.TextBox txt_OutputPath;
+        private System.Windows.Forms.Label lbl_FileName;
+        private System.Windows.Forms.Label lbl_InputPath;
+        private System.Windows.Forms.Label lbl_OutputPath;
+        private System.Windows.Forms.ListBox lb_Status;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_DialogInput;
+        private System.Windows.Forms.Button btn_DialogOutput;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Input;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Output;
+        private System.Windows.Forms.ListBox lb_Error;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ AutoFileCopyApplication/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoFileCopyApplication
+{
+    public partial class Form1 : Form
+    {
+        bool hidden = false;
+        System.Windows.Forms.Timer timer1;
+        bool isTimerStartFirst = true;
+        bool isFileCopied = false;
+        DateTime firstStarted = DateTime.Now;
+        Int32 timeMiliseconds = 6000;
+        static bool isPathFromFile = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isPathFromFile == false)
+                {
+                    using (StreamWriter writetext = new StreamWriter("readpath.txt"))
+                    {
+                        writetext.WriteLine(txt_FileName.Text);
+                        writetext.WriteLine(txt_InputPath.Text);
+                        writetext.WriteLine(txt_OutputPath.Text);
+                    }
+                }
+
+                //if (txt_FileName.Text != "" && txt_InputPath.Text != "" && txt_OutputPath.Text != "")
+                if (txt_FileName.Text != "" && txt_InputPath.Text != "" && txt_OutputPath.Text != "")
+                {
+                }
+                else
+                {
+                    string error = (txt_FileName.Text == "") ? "Enter File Name \n" : "";
+                    error += (txt_InputPath.Text == "") ? "Select Input Path \n" : "";
+                    error += (txt_OutputPath.Text == "") ? "Select Output Path \n" : "";
+                    MessageBox.Show(error, "Error");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message, "Start Button");
+            }
+        }
+        private void StartCopy()
+        {
+            try
+            {
+                lb_Status.Items.Add("---------------------------------");
+                lb_Status.Items.Add(DateTime.Now + " - Checking for files...");
+                DirectoryInfo dir = new DirectoryInfo(txt_InputPath.Text);
+                FileInfo[] files = dir.GetFiles(txt_FileName.Text);
+                int fileCount = 0;
+                Thread t = new Thread(new ThreadStart(delegate
+                {
+                    foreach (FileInfo f in files)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            lb_Error.BackColor = Color.Green;
+                        });
+                        string name = f.Name;
+                        string dateTime = f.LastWriteTime.ToString("dd/MM/yyyy HH:mm:ss");
+                        DateTime lastTime = DateTime.ParseExact(dateTime, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                        DateTime fileDate = lastTime;
+                        DateTime systemDate = DateTime.Now;
+                        TimeSpan time = systemDate - fileDate;
+
+                        if (time.TotalHours < 2)
+                        //if (time.TotalDays < 20)
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                lb_Status.Items.Add(DateTime.Now + " - Copying started...");
+                            });
+                            //CopyFile(name, txt_InputPath.Text, txt_OutputPath.Text);
+                            File.Copy(txt_InputPath.Text + "\\" + name, txt_OutputPath.Text + "\\" + name, true);                            
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                lb_Status.Items.Add(DateTime.Now + " - " + name + " - copied successfully");
+                                using (StreamWriter writetext = new StreamWriter("copiedfiles.txt", true))
+                                {
+                                    writetext.WriteLine(DateTime.Now + " - File: " + name + " copied successfully");
+                                }
+                                lb_Status.Items.Add(DateTime.Now + " - Copying ended...");
+                                lb_Status.Items.Add("---------------------------------");
+                            });
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                fileCount++;
+                            });
+                        }                       
+                    }
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        lb_Error.BackColor = Color.Honeydew;
+                        lb_Status.Items.Add("--------------waiting-------------");
+                    });
+                    Thread.Sleep(timeMiliseconds);
+                }));
+                
+                t.IsBackground = true;
+                t.Start();
+
+                if (fileCount == 0)
+                    isFileCopied = false;
+                else
+                    isFileCopied = true;
+
+                SetTimerInterval();
+                TimeSpan timeSpan = TimeSpan.FromMilliseconds(timeMiliseconds);
+                lb_Status.Items.Add("Next File Check in " + timeSpan.TotalHours.ToString() + " Hours.");
+                lb_Status.Items.Add("---------------------------------");
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message, "Copy Thread");
+            }
+        }
+        private string CopyFile(string fileName, string inputPath, string outputPath)
+        {
+            string returnMessage = "";
+            try
+            {
+
+               
+
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message, "Copy File");
+                returnMessage = "Error: " + ex.Message;
+            }
+            return returnMessage;
+        }
+
+        private void btn_DialogInput_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = folderBrowserDialog_Input.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    //
+                    // The user selected a folder and pressed the OK button.
+                    // We print the number of files found.
+                    //
+                    txt_InputPath.Text = folderBrowserDialog_Input.SelectedPath.ToString();
+                    //string[] files = Directory.GetFiles(folderBrowserDialog_Input.SelectedPath);
+                    //MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message, "Input Dialog");
+            }
+        }
+
+        private void btn_DialogOutput_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = folderBrowserDialog_Output.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    //
+                    // The user selected a folder and pressed the OK button.
+                    // We print the number of files found.
+                    //
+                    txt_OutputPath.Text = folderBrowserDialog_Output.SelectedPath.ToString();
+                    //string[] files = Directory.GetFiles(folderBrowserDialog_Input.SelectedPath);
+                    //MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(ex.Message, "Output Dialog");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+
+            this.ShowInTaskbar = false;         
+
+            try
+            {
+                const Int32 BufferSize = 128;
+                using (var fileStream = File.OpenRead("readpath.txt"))
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+                {
+                    String line;
+                    int counter = 0;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        if (counter == 0)
+                            txt_FileName.Text = line;
+                        else if (counter == 1)
+                            txt_InputPath.Text = line;
+                        else if (counter == 2)
+                            txt_OutputPath.Text = line;
+
+                        counter++;
+                    }
+                    if (counter == 2)
+                        isPathFromFile = true;
+                    // Process line                    
+                }
+                InitTimer();
+            }
+            catch (Exception ex)
+            {
+                lb_Error.Items.Clear();
+                ErrorLog(ex.Message, "Form Load");
+            }
+        }
+
+
+        private void ErrorLog(string exception, string level)
+        {
+            lb_Error.Items.Insert(0, DateTime.Now + " - Level: " + level + " - Exception: " + exception);
+            using (StreamWriter writetext = new StreamWriter("error.txt"))
+            {
+                writetext.WriteLine(DateTime.Now + " - Level: " + level + " - Exception: " + exception);
+            }
+        }
+        public void InitTimer()
+        {
+            StartCopy();
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            timer1.Interval = timeMiliseconds;
+            //timer1.Interval = (isTimerStartFirst == true) ? 200 : 60000; // in miliseconds
+            timer1.Start();
+        }
+
+        private void SetTimerInterval()
+        {
+            //timeMiliseconds = (isFileCopied != true) ? 60000 * 30 : (60000 * 60) * 12; // 60000 miliseconds = 1 min 
+            timeMiliseconds = (60000 * 60)*2; // 60000 miliseconds = 1 min             
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StartCopy();
+
+            if (lb_Status.Items.Count > 1500)
+            {
+                lb_Status.Items.Clear();
+                lb_Status.Items.Add(DateTime.Now + " - Log cleared");
+            }
+        }
+
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            notifyIcon1.Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            notifyIcon1.Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(300);
+            }
+        }
+    }
+}
